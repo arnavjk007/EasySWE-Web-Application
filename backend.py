3cbf354be9f83fdc874d5f120a72ac487b235ef5
Arnav Kumar <144872754+arnavjk007@users.noreply.github.com>
2024-04-07 18:14:39 -0700

    Updated

--- backend.py
+++ backend.py
@@ -15,7 +15,7 @@ internship_values = []
 
 def scrape():
     #print("called scrape!")
-    url = "https://github.com/SimplifyJobs/Summer2024-Internships/blob/dev/README.md"
+    url = <URL>
     response = requests.get(url)
     global internship_values
     #internship_values = parseHTML(response.content)
